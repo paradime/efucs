51d2c3bb277ed24aba48fbfaf32bda81ca5bd281
Bryon Wilkins <Brw4824@rit.edu>
2016-08-29 20:00:50 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+/** 
+* Lab 1 - GPIO Blinky
+* - Drive an LED by GPIO
+* - Developer - 
+* - Date - 
+* 
+*/
+#include "gpio.h"
+
+// Write a function that will delay for n loops
+// to satisfy timing constraints given in lab. 
+// ....beware compiler optimization....
+void delay(uint32_t count)
+{
+	
+}
+
+int main()
+{
+	// Select output mode and which pin to drive
+        
+    while (1)
+    {       
+        //toggle clear register for the chosen pin
+        
+        //apply a delay
+        
+        //toggle set register for the chosen pin
+        
+        //apply a delay
+    }
+    
+    return 0;
+}
